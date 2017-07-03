6964a3134cae57677d635324744cc8a616d09b91
Ivan Magda <imagda15@gmail.com>
2017-07-04 00:15:59 +0300

    chore: Initial Commit

---
+++ IMNetworkReachability/IMNetworkReachability.h
@@ -0,0 +1,19 @@
+//
+//  IMNetworkReachability.h
+//  IMNetworkReachability
+//
+//  Created by Ivan Magda on 04/07/2017.
+//  Copyright © 2017 Ivan Magda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IMNetworkReachability.
+FOUNDATION_EXPORT double IMNetworkReachabilityVersionNumber;
+
+//! Project version string for IMNetworkReachability.
+FOUNDATION_EXPORT const unsigned char IMNetworkReachabilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IMNetworkReachability/PublicHeader.h>
+
+
